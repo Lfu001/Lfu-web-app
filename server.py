2217cbe3872a9a5fbdee15f766e4615ed032f676
Lfu <goodspeed913@gmail.com>
2021-07-22 14:04:55 +0900

    Formatted the code

--- server.py
+++ server.py
@@ -3,20 +3,24 @@ import math
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def root():
-    myname = request.args.get("name")
+    # myname = request.args.get("name")
     return render_template("home.html")
+
 
 @app.route("/area")
 def link():
     return render_template("area.html")
+
 
 @app.route("/area/circle")
 def area_circle():
     radius = request.args.get("r", default=None, type=float)
     answer = round(radius**2 * math.pi, 2)
     return render_template("circle.html", radius=radius, answer=answer)
+
 
 @app.route("/area/rectangle")
 def area_rectangle():
@@ -25,15 +29,18 @@ def area_rectangle():
     answer = round(a * b, 2)
     return render_template("rectangle.html", a=a, b=b, answer=answer)
 
-#@app.route("/area/triangle")
-#def area_triangle():
+
+# @app.route("/area/triangle")
+# def area_triangle():
 #    a = request.args.get("a", default=None, type=float)
 #    answer = round(0.5 * a * a * (math.sqrt(3) * 0.5), 2)
 #    return render_template("triangle.html", a=a, answer=answer)
 
+
 @app.route("/hello")
 def hello():
     return "hello"
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=9761)
