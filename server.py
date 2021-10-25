8bc64eb303debd96fba3f7f51325b2df09d7566d
Lfu <goodspeed913@gmail.com>
2021-10-25 19:33:51 +0900

    update port setting

--- server.py
+++ server.py
@@ -50,5 +50,5 @@ def hello():
 
 
 if __name__ == '__main__':
-    # port = int(os.environ.get("PORT", 9761))
-    app.run(debug=True, port=int(os.environ['PORT']), host="0.0.0.0")
+    port = int(os.environ.get("PORT", 9761))
+    app.run(debug=True, port=port, host="0.0.0.0")
