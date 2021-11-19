e8e5d5df10ebb2fbde99c503f5eb439a19b2bf4d
Lfu <goodspeed913@gmail.com>
2021-11-19 19:15:15 +0900

    Added user ID and timestamp fields to the database.

--- historyDB.py
+++ historyDB.py
@@ -6,24 +6,26 @@ def addHistory(shape, data):
     con = sqlite3.connect(os.path.join(os.path.dirname(__file__), "history.sqlite3"))
     cur = con.cursor()
 
+    values = ", ".join([str(d) if not isinstance(d, str) else "'" + d + "'" for d in data])
+
     if shape == "rectangle":
-        cur.execute("insert into rectHistory values(" + ", ".join([str(d) for d in data]) + ");")
+        cur.execute("insert into rectHistory values(" + values + ");")
 
     if shape == "circle":
-        cur.execute("insert into circHistory values(" + ", ".join([str(d) for d in data]) + ");")
+        cur.execute("insert into circHistory values(" + values + ");")
 
     con.commit()
     con.close()
 
 
-def getHistory(shape):
+def getHistory(shape, user_id):
     con = sqlite3.connect(os.path.join(os.path.dirname(__file__), "history.sqlite3"))
     cur = con.cursor()
 
     if shape == "rectangle":
-        cur.execute("select * from rectHistory;")
+        cur.execute("select a, b, result, timestamp from rectHistory where user_id = '" + user_id + "' order by timestamp desc;")
 
     if shape == "circle":
-        cur.execute("select * from circHistory;")
+        cur.execute("select r, result, timestamp from circHistory  where user_id = '" + user_id + "' order by timestamp desc;")
 
     return cur.fetchall()
